68c22f269b589e0a8974b4f68edc8a2000d98c30
ChaoCeciliaChen <ccecilia@umich.edu>
2023-11-22 22:24:01 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Classifying Iris Flowers')
+st.title('Flight Delay Prediction Models')
 st.markdown("Cecilia's first ML dashboard")
